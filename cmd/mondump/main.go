4520c1e402255e2e2afc234191cfce855d9cffb0
christopher <post@c-ha.de>
2019-12-16 17:00:49 +0100

    use dynamic request system from disresc/lib

--- cmd/mondump/main.go
+++ cmd/mondump/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"time"
+
 	drModels "github.com/disresc/lib/models"
 	drReceiver "github.com/disresc/lib/receiver"
-	"os"
 )
 
 func handle(event *drModels.Event) {
@@ -14,17 +16,43 @@ func handle(event *drModels.Event) {
 func main() {
 	name, found := os.LookupEnv("name")
 	if !found {
-		name = "transmitter"
+		name = "mondump"
 	}
-	/*topic, found := os.LookupEnv("topic")
-	if !found {
-		topic = "monitoring"
-	}*/
 
 	receiver := drReceiver.NewService(name)
 	receiver.Start()
+
+	sendPeriodicRequest(receiver, "hosts", "kvmtop-cpu")
+	sendPeriodicRequest(receiver, "ves", "kvmtop-cpu")
+
 	for {
 		event := <-receiver.EventChannel()
 		handle(event)
 	}
+}
+
+func sendPeriodicRequest(receiver *drReceiver.Service, source string, transmitter string) {
+	ticker := time.NewTicker(5 * time.Second)
+	//done := make(chan bool)
+	sendRequest(receiver, source, transmitter)
+	go func() {
+		for {
+			select {
+			//case <-done:
+			//	return
+			case <-ticker.C:
+				sendRequest(receiver, source, transmitter)
+			}
+		}
+	}()
+}
+
+func sendRequest(receiver *drReceiver.Service, source string, transmitter string) {
+	request := drModels.Request{
+		Timeout:     15,
+		Source:      source,
+		Transmitter: transmitter,
+		Interval:    10,
+	}
+	receiver.Request(&request)
 }
--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module mondump
 go 1.13
 
 require (
-	github.com/disresc/lib v0.0.0-20191202124906-9d73ef7f963f
-	github.com/go-log/log v0.1.0
-	github.com/golang/protobuf v1.3.2
-	github.com/micro/examples v0.2.0
-	github.com/micro/go-micro v1.17.1
-	github.com/pborman/uuid v1.2.0
-	rsc.io/quote v1.5.2
+	github.com/disresc/lib v0.0.0-20191216155224-4e699beba30d
+	github.com/golang/mock v1.3.1 // indirect
+	github.com/google/btree v1.0.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.9.2 // indirect
+	github.com/onsi/ginkgo v1.8.0 // indirect
+	github.com/onsi/gomega v1.5.0 // indirect
 )
+
+// replace github.com/disresc/lib v0.0.0-20191202124906-9d73ef7f963f => /home/christopher/Documents/UNI/workspace/kvmtop-modules/github.com/disresc/lib
+
+// replace github.com/micro/go-micro v1.17.1 => /home/christopher/Documents/UNI/workspace/kvmtop-modules/github.com/cha87de/go-micro
